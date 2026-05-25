8e55c8d88c68e27f95a942ad83a27915ab851b57
maluchaves91 <95364349+maluchaves91@users.noreply.github.com>
2026-05-25 16:21:00 -0300

    testes em aula

--- meu-primeiro-java/src/App.java
+++ meu-primeiro-java/src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        try (Scanner entrada = new Scanner(System.in)) {
+
+
+            System.out.println("Digite o valor de A:");
+            int a = entrada.nextInt();
+
+
+            System.out.println("Digite o valor de B:");
+            int b = entrada.nextInt();
+
+
+            int resultado = soma(a, b);
+
+
+            System.out.println("A soma é: " + resultado);
+        }
     }
 }
