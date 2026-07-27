7568cf0e8682404528e295cc4293600f2f97ba49
maluchaves91 <95364349+maluchaves91@users.noreply.github.com>
2026-07-27 16:32:38 -0300

    projeto DIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println();
+        System.out.println(
+            "Olá " + nomeCliente + 
+            ", obrigado por criar uma conta em nosso banco, sua agência é " +
+            agencia + 
+            ", conta " +
+            numero + 
+            "e seu saldo R$ " + 
+            saldo + 
+            " já está disponível na conta"
+        );
+
+        scanner.close();
+
+        
     }
 }
